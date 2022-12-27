99d94d4d9ba28a74772084761d4e827225aeb8e0
cn2770345524 <2770345524@qq.com>
2022-12-27 21:51:13 +0800

    初始化 cloud-api-commons模块

---
+++ cloud-api-commons/src/main/java/com/liuhao/cloud/commons/entity/CommonResult.java
@@ -0,0 +1,24 @@
+package com.liuhao.cloud.commons.entity;
+
+import lombok.Data;
+
+@Data
+public class CommonResult<T> {
+
+    private Integer code;
+    private String message;
+    private T data;
+
+    public CommonResult() {
+    }
+
+    public CommonResult(Integer code, String message) {
+        this(code, message, null);
+    }
+
+    public CommonResult(Integer code, String message, T data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+}
---
+++ cloud-api-commons/src/main/java/com/liuhao/cloud/commons/entity/Payment.java
@@ -0,0 +1,14 @@
+package com.liuhao.cloud.commons.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment {
+
+    private Long id;
+    private String serial;
+}
