4cb42dd93ad10e812410dded0fd8c34bd134ac3e
cn2770345524 <2770345524@qq.com>
2022-12-28 21:32:04 +0800

    搭建eureka注册中心server端

---
+++ cloud-eureka-server7001/src/main/java/com/liuhao/cloud/eureka/server/EurekaServerApplication7001.java
@@ -0,0 +1,14 @@
+package com.liuhao.cloud.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication7001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication7001.class, args);
+    }
+}
