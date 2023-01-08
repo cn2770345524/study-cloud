46bd773577a99398ed261e4fe096f6f75db80ec1
cn2770345524 <2770345524@qq.com>
2023-01-08 10:40:19 +0800

    更改cloud-eureka-server模块，复用

--- cloud-eureka-server/src/main/java/com/liuhao/cloud/eureka/server/EurekaServerApplication.java
+++ cloud-eureka-server/src/main/java/com/liuhao/cloud/eureka/server/EurekaServerApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class EurekaServerApplication7001 {
+public class EurekaServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApplication7001.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 }
