5d2fa4fc1f584a80f41ba06f72365fa0925e5edf
cn2770345524 <2770345524@qq.com>
2023-01-08 13:31:49 +0800

    初步使用注解discoveryClient与enableDiscoveryClient

--- cloud-consumer-order80/src/main/java/com/liuhao/cloud/consumer/controller/DefaultController.java
+++ cloud-consumer-order80/src/main/java/com/liuhao/cloud/consumer/controller/DefaultController.java
@@ -1,13 +1,19 @@
 package com.liuhao.cloud.consumer.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.liuhao.cloud.commons.entity.CommonResult;
 import com.liuhao.cloud.commons.entity.Payment;
+import org.apache.commons.collections4.MapUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RequestMapping("/consumer")
 @RestController
@@ -21,6 +27,9 @@ public class DefaultController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @PostMapping("/payment/create")
     public CommonResult<Payment> create(@RequestBody Payment payment) {
         return restTemplate.postForObject(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
@@ -30,5 +39,18 @@ public class DefaultController {
     public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
         logger.info("com.liuhao.cloud.consumer.controller.DefaultController.getPayment");
         return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+    }
+
+    @GetMapping("/payment/discovery")
+    public Object getDiscoveryInfo() {
+        List<String> services = discoveryClient.getServices();
+        for (String service : services) {
+            logger.info("service name:{}", service);
+        }
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance instance : instances) {
+            logger.info("instance of :{}", JSON.toJSONString(instance));
+        }
+        return discoveryClient;
     }
 }
