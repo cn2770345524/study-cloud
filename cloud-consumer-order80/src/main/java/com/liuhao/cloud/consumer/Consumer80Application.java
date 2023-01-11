84dafefe776faf95ab957854639eb1f50024c7c8
cn2770345524 <2770345524@qq.com>
2023-01-11 22:37:35 +0800

    MyRuleSetting用于配置ribbon负载均衡使用的算法，默认是使用的轮询算法，我们可以通过配置来修改负载均衡所使用的的算法
    ribbon的负载均衡算法
    所谓的负载均衡策略，就是当A服务调用B服务时，此时B服务有多个实例，这时A服务以何种方式来选择调用的B实例，ribbon可以选择以下几种负载均衡策略（ribbon的所有负载均衡算法均是com.netflix.loadbalancer.IRule接口的实现）。
    com.netflix.loadbalancer.RandomRule：从提供服务的实例中以随机的方式；
    com.netflix.loadbalancer.RoundRobinRule：以线性轮询的方式，就是维护一个计数器，从提供服务的实例中按顺序选取，第一次选第一个，第二次选第二个，以此类推，到最后一个以后再从头来过；
    com.netflix.loadbalancer.RetryRule：在RoundRobinRule的基础上添加重试机制，即在指定的重试时间内，反复使用线性轮询策略来选择可用实例；
    com.netflix.loadbalancer.WeightedResponseTimeRule：对RoundRobinRule的扩展，响应速度越快的实例选择权重越大，越容易被选择；
    com.netflix.loadbalancer.BestAvailableRule：选择并发较小的实例；
    com.netflix.loadbalancer.AvailabilityFilteringRule：先过滤掉故障实例，再选择并发较小的实例；
    com.netflix.loadbalancer.ZoneAwareLoadBalancer：采用双重过滤，同时过滤不是同一区域的实例和故障实例，选择并发较小的实例。
    
    注意：
    不能将对于ribbon的配置放在能被ComponentScan扫描的包下(既springboot启动类所在包以下)，那样做会导致配置对全局生效
    最好 放在上层不同的包名下

--- cloud-consumer-order80/src/main/java/com/liuhao/cloud/consumer/Consumer80Application.java
+++ cloud-consumer-order80/src/main/java/com/liuhao/cloud/consumer/Consumer80Application.java
@@ -1,9 +1,12 @@
 package com.liuhao.cloud.consumer;
 
+import com.liuhao.cloud.ribbon.settings.MyRuleSetting;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE", configuration = MyRuleSetting.class) // 启动该服务时去加载自定义的ribbon配置
 @EnableDiscoveryClient
 @SpringBootApplication
 public class Consumer80Application {
