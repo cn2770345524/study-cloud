408e1bb67d33d678266893f064c5aa835224fb8e
cn2770345524 <2770345524@qq.com>
2023-01-16 21:56:34 +0800

    创建consumer-openfeign模块用于测试使用openfeign

---
+++ cloud-consumer-OpenFeign-order80/src/main/java/com/liuhao/cloud/ribbon/settings/MyLoadBalanceRule.java
@@ -0,0 +1,97 @@
+package com.liuhao.cloud.ribbon.settings;
+
+import com.netflix.client.config.IClientConfig;
+import com.netflix.loadbalancer.AbstractLoadBalancerRule;
+import com.netflix.loadbalancer.ILoadBalancer;
+import com.netflix.loadbalancer.Server;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class MyLoadBalanceRule extends AbstractLoadBalancerRule {
+
+    private static Logger log = LoggerFactory.getLogger(MyLoadBalanceRule.class);
+
+    private AtomicInteger nextServerCyclicCounter;
+
+    public MyLoadBalanceRule() {
+        nextServerCyclicCounter = new AtomicInteger(0);
+    }
+
+    public MyLoadBalanceRule(ILoadBalancer lb) {
+        this();
+        setLoadBalancer(lb);
+    }
+
+    public Server choose(ILoadBalancer lb, Object key) {
+        log.info("com.liuhao.cloud.ribbon.settings.MyLoadBalanceRule.choose method:{}",key);
+        if (lb == null) {
+            log.warn("no load balancer");
+            return null;
+        }
+
+        Server server = null;
+        int count = 0;
+        while (server == null && count++ < 10) {
+            List<Server> reachableServers = lb.getReachableServers();
+            List<Server> allServers = lb.getAllServers();
+            int upCount = reachableServers.size();
+            int serverCount = allServers.size();
+
+            if ((upCount == 0) || (serverCount == 0)) {
+                log.warn("No up servers available from load balancer: " + lb);
+                return null;
+            }
+
+            int nextServerIndex = incrementAndGetModulo(serverCount);
+            server = allServers.get(nextServerIndex);
+
+            if (server == null) {
+                /* Transient. */
+                Thread.yield();
+                continue;
+            }
+
+            if (server.isAlive() && (server.isReadyToServe())) {
+                return (server);
+            }
+
+            // Next.
+            server = null;
+        }
+
+        if (count >= 10) {
+            log.warn("No available alive servers after 10 tries from load balancer: "
+                    + lb);
+        }
+        return server;
+    }
+
+    /**
+     * Inspired by the implementation of {@link AtomicInteger#incrementAndGet()}.
+     *
+     * @param modulo The modulo to bound the value of the counter.
+     * @return The next value.
+     */
+    private int incrementAndGetModulo(int modulo) {
+        for (; ; ) {
+            int current = nextServerCyclicCounter.get();
+            int next = (current + 1) % modulo;
+            if (nextServerCyclicCounter.compareAndSet(current, next)) {
+                return next;
+            }
+        }
+    }
+
+    @Override
+    public void initWithNiwsConfig(IClientConfig iClientConfig) {
+
+    }
+
+    @Override
+    public Server choose(Object key) {
+        return choose(getLoadBalancer(), key);
+    }
+}
