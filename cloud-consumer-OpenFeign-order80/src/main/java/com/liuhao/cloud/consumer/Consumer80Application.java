c9178bea52abb0c76c7a501708bd0d947d9a84fd
cn2770345524 <2770345524@qq.com>
2023-01-21 17:26:22 +0800

    1.因为主要测试openFeign所以不需要自定义配置ribbon的负载均衡策略，删除掉不必要的配置

--- cloud-consumer-OpenFeign-order80/src/main/java/com/liuhao/cloud/consumer/Consumer80Application.java
+++ cloud-consumer-OpenFeign-order80/src/main/java/com/liuhao/cloud/consumer/Consumer80Application.java
@@ -1,13 +1,10 @@
 package com.liuhao.cloud.consumer;
 
-import com.liuhao.cloud.ribbon.settings.MyRuleSetting;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-// 启动该服务时去加载自定义的ribbon配置，注意服务名的大小写问题，有可能因为大小写问题导致无法成功配置
-@RibbonClient(name = "cloud-payment-service", configuration = MyRuleSetting.class)
 @EnableDiscoveryClient
 @SpringBootApplication
 public class Consumer80Application {
