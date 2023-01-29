ed61dc46a4c65f926f8d6984acf4d1cd302926fc
cn2770345524 <2770345524@qq.com>
2023-01-29 20:59:49 +0800

    hystrix-payment模块设置服务降级

--- cloud-provider-hystrix-payment/src/main/java/com/liuhao/cloud/HystrixPaymentServiceApplication.java
+++ cloud-provider-hystrix-payment/src/main/java/com/liuhao/cloud/HystrixPaymentServiceApplication.java
@@ -2,8 +2,10 @@ package com.liuhao.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableCircuitBreaker
 @EnableEurekaClient
 @SpringBootApplication
 public class HystrixPaymentServiceApplication {
--- cloud-provider-hystrix-payment/src/main/java/com/liuhao/cloud/controller/PaymentController.java
+++ cloud-provider-hystrix-payment/src/main/java/com/liuhao/cloud/controller/PaymentController.java
@@ -3,6 +3,8 @@ package com.liuhao.cloud.controller;
 import com.liuhao.cloud.commons.entity.CommonResult;
 import com.liuhao.cloud.commons.entity.Payment;
 import com.liuhao.cloud.service.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +25,10 @@ public class PaymentController {
     @Autowired
     private PaymentService paymentService;
 
+    // 设置失败的兜底犯法，设置方法超时的时间timeout属性
+    @HystrixCommand(fallbackMethod = "selectOne_fallback", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000")
+    })
     @GetMapping("get/{id}")
     public CommonResult<Payment> selectOne(@PathVariable("id") Long id) {
         Payment payment = this.paymentService.queryById(id);
@@ -42,11 +48,15 @@ public class PaymentController {
         return new CommonResult<>(200, "port:{" + serverPort + "}", insert);
     }
 
-    @RequestMapping(method = RequestMethod.GET,value = "/uuid")
+    @RequestMapping(method = RequestMethod.GET, value = "/uuid")
     public CommonResult<String> getUUID() {
         String uuid = UUID.randomUUID().toString();
         logger.info("uuid:{}", uuid);
         return new CommonResult<>(200, uuid, null);
+    }
+
+    public CommonResult<Payment> selectOne_fallback(Long id) {
+        return new CommonResult<>(500,"服务调用失败，返回默认标识");
     }
 
 }
