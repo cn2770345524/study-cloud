df215330053e2d25b2b48d49ae7dbda7694d7557
cn2770345524 <2770345524@qq.com>
2023-02-02 22:12:21 +0800

    对需要被监控的HystrixPaymentService模块进行配置，以便hystix-dashboard模块监控

---
+++ cloud-provider-hystrix-payment/src/main/java/com/liuhao/cloud/config/HystrixDashBoardConfiguration.java
@@ -0,0 +1,23 @@
+package com.liuhao.cloud.config;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HystrixDashBoardConfiguration {
+    /**
+     *  Hystrix dashboard 监控界面必须配置
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");//访问路径
+        registrationBean.setName("hystrix.stream");
+        return registrationBean;
+    }
+}
