9cd21313372bc8412f2122deaae80c5516ccfe9a
cn2770345524 <2770345524@qq.com>
2023-02-11 21:33:26 +0800

    通过添加actuator模块，得以让客户端不需要重启即可手动刷新配置信息 注意：本例需要spring-boot-starter-actuator模块 手动刷新配置需要发送一条post请求给我客户端：spring-boot-starter-actuator

--- cloud-config-client/src/main/java/com/liuhao/config/client/controller/TestController.java
+++ cloud-config-client/src/main/java/com/liuhao/config/client/controller/TestController.java
@@ -1,9 +1,12 @@
 package com.liuhao.config.client.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// 读取配置中心指定配置文件的内容，并展示到页面
+@RefreshScope //为了让动态（手动）的获取最新的git 配置，在添加 actuator 监控加载 RefreshScope，
 @RequestMapping("/config/test")
 @RestController
 public class TestController {
