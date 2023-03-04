d73719118f42b5d45290331e8ce7a300bc6e1828
cn2770345524 <2770345524@qq.com>
2023-03-04 22:47:01 +0800

    测试sentinel,添加sentinel控制台

--- cloud-nacos-consumer-order/src/main/java/com/liuhao/cloud/consumer/controller/DefaultController.java
+++ cloud-nacos-consumer-order/src/main/java/com/liuhao/cloud/consumer/controller/DefaultController.java
@@ -1,5 +1,9 @@
 package com.liuhao.cloud.consumer.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,15 +15,25 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class DefaultController {
 
+    private static Logger log=LoggerFactory.getLogger(DefaultController.class);
+
     @Value("${server.port}")
     public Integer serverPort;
 
     @Autowired
     private RestTemplate restTemplate;
 
+    @SentinelResource("consumer/get")
     @RequestMapping(path = "/get",method = RequestMethod.GET)
     public String getId(){
         String url="http://cloud-nacos-payment-service/provider/get";
-        return restTemplate.getForObject(url,String.class);
+        String result = restTemplate.getForObject(url, String.class);
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        log.info("passed");
+        return result;
     }
 }
